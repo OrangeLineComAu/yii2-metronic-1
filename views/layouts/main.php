03b2298b9640c2d22254e165cb8331086c0dd9e4
Su Anli <anli@euqol.com>
2015-08-12 16:54:10 +0800

    added assets, gridview, navbar portlet widgets and js

---
+++ views/layouts/main.php
@@ -0,0 +1,67 @@
+<?php
+
+use anli\metronic\assets\MainAsset;
+use yii\helpers\Html;
+use yii\widgets\Breadcrumbs;
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+MainAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body class="page-sidebar-closed-hide-logo page-container-bg-solid page-sidebar-closed-hide-logo page-boxed page-header-fixed page-footer-fixed">
+
+<?php $this->beginBody() ?>
+
+<!-- BEGIN HEADER -->
+<?= $this->render('_header') ?>
+<!-- END HEADER -->
+
+<div class="clearfix"></div>
+
+<div class="page-container">
+
+    <!-- BEGIN SIDEBAR -->
+	<?= $this->render('_sidebar') ?>
+	<!-- END SIDEBAR -->
+
+    <div class="page-content-wrapper">
+        <div class="page-content">
+            <!-- BEGIN PAGE HEADER-->
+			<h3 class="page-title">
+			<?= Html::encode($this->title) ?> <small></small>
+			</h3>
+			<div class="page-bar">
+				<?= Breadcrumbs::widget([
+					'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+					'options' => ['class' => 'page-breadcrumb '],
+					'itemTemplate' => "<li>{link}<i class=\"fa fa-angle-right\"></i></li>\n",
+				]) ?>
+			</div>
+			<!-- END PAGE HEADER-->
+
+            <!-- BEGIN PAGE CONTENT-->
+            <?= $content ?>
+            <!-- END PAGE CONTENT-->
+        </div>
+    </div>
+</div>
+
+<!-- BEGIN FOOTER -->
+<?= $this->render('_footer') ?>
+<!-- END FOOTER -->
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
---
+++ widgets/GridView.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * @link http://www.euqol.com/
+ * @copyright Copyright (c) 2015 Su Anli
+ * @license http://www.euqol.com/license/
+ */
+
+namespace anli\metronic\widgets;
+
+use Yii;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
+use yii\bootstrap\Widget;
+use yii\bootstrap\BootstrapPluginAsset;
+use yii\grid\GridView as BaseGridView;
+
+/**
+ * This is the grid view widget class.
+ *
+ * ```php
+ * use anli\metronic\widgets\GridView;
+ *
+ * GridView::widget([
+ *      'dataProvider' => $dataProvider,
+ *      'columns' => $columns,
+ * ]);
+ * ...
+ * <?php Portlet::end(); ?>
+ * ```
+ *
+ * @author Su Anli <anli@euqol.com>
+ * @since 1.0.0
+ */
+class GridView extends Widget
+{
+    /**
+     * @param string
+     */
+    public $id = '';
+
+    /**
+     * @param mixed
+     */
+    public $dataProvider;
+
+    /**
+     * @param array
+     */
+    public $columns = [];
+
+    /**
+     * @param array
+     */
+    protected $configs = [
+        'options' => ['class' => 'table-scrollable table-scrollable-borderless'],
+        'tableOptions' => ['class' => 'table table-hover table-light'],
+        'summary' => '',
+        'headerRowOptions' => ['class' => 'uppercase'],
+    ];
+
+    /**
+     * Initializes the widget.
+     */
+    public function init()
+    {
+        parent::init();
+
+        $this->configs = array_replace_recursive($this->configs, ['dataProvider' => $this->dataProvider]);
+        $this->configs = array_replace_recursive($this->configs, ['columns' => $this->columns]);
+        $this->configs = array_replace_recursive($this->configs, ['dataProvider' => $this->dataProvider]);
+
+        if ('' != $this->id) {
+            $this->configs = array_replace_recursive($this->configs, ['id' => $this->id]);
+        }
+    }
+
+    /**
+     * Renders the widget.
+     */
+    public function run()
+    {
+        echo BaseGridView::widget($this->configs);
+    }
+}
